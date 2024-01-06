a62d89cc19fc6b07181eb459549ff010f1683025
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-01-06 08:28:17 -0600

    Shortens color setting func name

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,4 @@
-function SetMyColors(color)
+function SetColors(color)
     color = color or 'rose-pine'
     vim.cmd.colorscheme(color)
 
@@ -6,4 +6,4 @@ function SetMyColors(color)
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-SetMyColors()
+SetColors()
