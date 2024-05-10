fa30cb7d5fd6bfad2ff7f0db335658715e43b516
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-05-09 21:18:06 -0500

    Adds go support to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "html", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "jsdoc", "rust", "sql", "scss", "svelte" },
+  ensure_installed = { "html", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "jsdoc", "rust", "sql", "scss", "svelte", "go" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
