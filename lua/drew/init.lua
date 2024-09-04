8618a42a5ee56b19c52bbac998a53dddb0c79263
Drew_Pfeiler <drewcpfeiler@gmail.com>
2024-09-03 21:34:10 -0500

    Removes autocmd for directory change

--- lua/drew/init.lua
+++ lua/drew/init.lua
@@ -1,15 +1,3 @@
 require("drew.nvimtree");
 require("drew.remap");
 require("drew.set");
-
--- Determines the start path for neovim-qt, defaults to user folder
-local os = require("os")
-
-local path_to_desktop = os.getenv("USERPROFILE") .. ""
-
-local vim_enter_group = vim.api.nvim_create_augroup("vim_enter_group", { clear = true })
-
-vim.api.nvim_create_autocmd(
-    {"VimEnter"},
-    { pattern = "*", command = "cd " .. path_to_desktop, group = vim_enter_group }
-)
